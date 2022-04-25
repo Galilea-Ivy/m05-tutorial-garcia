77a49c8f6603f60802d8056c1e851675a3e60c13
Galilea-Ivy <ggarcia87@ivytech.edu>
2022-04-25 19:22:39 -0400

    created three vue pages

---
+++ src/views/CartPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>This is the shopping cart page</h1>
+</template>
+
+<script>
+export default {
+  name: "CartPage",
+};
+</script>
+
+<style>
+h1 {
+  color: red;
+}
+</style>
---
+++ src/views/ProductDetailPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>This is the products detail page</h1>
+</template>
+
+<script>
+export default {
+  name: "ProductsDetailPage",
+};
+</script>
+
+<style>
+h1 {
+  color: red;
+}
+</style>
