afb9caca8cee10b1b92e33d09bbfcc31fe7d21e3
Galilea-Ivy <ggarcia87@ivytech.edu>
2022-04-25 19:29:54 -0400

    created routes to the vue pages from the route index.js file

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import CartPage from '../views/CartPage.vue';
+import ProductsDetailPage from '../views/ProductDetailPage';
+import ProductsPage from '../views/ProductsPage';
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  { 
+    path: '/products',
+    name: 'Products',
+    component: ProductsPage,
+  }, {
+    path: '/products/:id',
+    name: 'ProductDetail',
+    component: ProductsDetailPage,
+  }, {
+    path: '/cart',
+    name: 'Cart',
+    component: CartPage,
+  }, {
+    path: "/",
+    redirect: "/products",
   }
 ]
 
